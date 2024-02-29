41276e7afca442baa4defda45e2fe142107b41ee
post-coder <gabrielspanu.dev@gmail.com>
2024-02-29 09:45:02 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+
+  components: {
+    AppTitle,
+  },
+
+  data() {
+    return {
+
+    }
+  },  
+}
+
+</script>
+
+<template>
+
+  <AppTitle></AppTitle>
+
+  Testo della pagina
+
+</template>
+
+<style>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,25 @@
+<script>
+
+export default {
+    name: "AppTitle",
+
+
+}
+
+</script>
+
+<template>
+
+    <h1>
+        La mia prima app con Vue
+    </h1>
+
+</template>
+
+<style>
+
+h1 {
+    text-align: center;
+}
+
+</style>
